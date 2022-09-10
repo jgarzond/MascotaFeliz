4770483fa9817ca3371998305f3b6129b61af772
jgarzond <jgarzond@unal.edu.co>
2022-09-10 12:51:29 -0500

    Version 1.0.3

--- MascotaFeliz.App/MascotaFeliz.App.Consola/Program.cs
+++ MascotaFeliz.App/MascotaFeliz.App.Consola/Program.cs
@@ -18,11 +18,9 @@ namespace MascotaFeliz.App.Consola
             //AddDueno();
             //AddVeterinario();
         }
-        /*private static void AddDueno()
-        {
-            var dueno = new Dueno
-            {
-                //Cedula = "1212",
+        //Métodos Dueño
+        private static void AddDueno(){
+            var dueno = new Dueno{
                 Nombres = "Juan",
                 Apellidos = "Sin Miedo",
                 Direccion = "Bajo un puente",
@@ -31,11 +29,25 @@ namespace MascotaFeliz.App.Consola
             };
             _repoDueno.AddDueno(dueno);
         }
-        private static void AddVeterinario()
-        {
-            var veterinario = new Veterinario
-            {
-                //Cedula = "1212",
+
+        public static void DeleteDueno(){
+           // _repoDueno.DeleteDueno(1);    Ingresar ID de la BD
+           // _repoDueno.DeleteDueno(2);
+        }
+        private static void BuscarDueno(int idDueno){
+            var dueno = _repoDueno.GetDueno(idDueno);
+            Console.WriteLine(dueno.Nombres + " " + dueno.Apellidos + " " + dueno.Direccion + " " + dueno.Telefono + " " + dueno.Correo);
+        }
+
+        private static void ListarDuenos(){
+            var duenos = _repoDueno.GetAllDuenos();
+            foreach (Dueno d in duenos){
+                Console.WriteLine(d.Nombres + " " + d.Apellidos + " " + d.Corr});
+            }
+        }
+    //Métodos Veterinario
+        private static void AddVeterinario(){
+            var veterinario = new Veterinario{
                 Nombres = "Juan",
                 Apellidos = "Sin Miedo",
                 Direccion = "Bajo un puente",
@@ -43,16 +55,24 @@ namespace MascotaFeliz.App.Consola
                 TarjetaProfesional = "25660"
             };
             _repoVeterinario.AddVeterinario(veterinario);
-        }*/
-        /*public static void DeleteDueno(){
-            _repoDueno.DeleteDueno(1);
-            _repoDueno.DeleteDueno(2);
-
         }
+        
         public static void DeleteVeterinario(){
-            _repoVeterinario.DeleteVeterinario(3);
+            //_repoVeterinario.DeleteVeterinario(3); Ingresar ID de la BD
         }
-        */
+
+        private static void BuscarVeterinario(int idVeterianrio){
+            var veterinario = _repoVeterinario.GetVeterinario(idVeterinario);
+            Console.WriteLine(veterinario.Nombres + " " + veterinario.Apellidos + " " + veterinario.Direccion + " " + veterinario.Telefono + " " + veterinario.TarjetaProfesional);
+        }
+
+        private static void ListarVeterinarios(){
+            var veterinarios = _repoVeterinario.GetAllVeterinarios();
+            foreach (Veterinario v in veterinarios){
+                Console.WriteLine(v.Nombres + " " + v.Apellidos + " " + v.TarjetaProfesional);
+            }
+        }
+        
 
 
     }
