0e4bb30249c42837574c1dca69f80e78c9468cb8
jgarzond <jgarzond@unal.edu.co>
2022-09-12 20:50:55 -0500

    Version 2.0

--- MascotaFeliz.App/MascotaFeliz.App.FrontEnd/Pages/Duenos/ListaDuenos.cshtml.cs
+++ MascotaFeliz.App/MascotaFeliz.App.FrontEnd/Pages/Duenos/ListaDuenos.cshtml.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MascotaFeliz.App.Dominio;
+using MascotaFeliz.App.Persistencia;
 
-namespace MascotaFeliz.App.FrontEnd.Pages
-{
-    public class ListaDuenosModel : PageModel
-    {
-        public void OnGet()
-        {
+namespace MascotaFeliz.App.FrontEnd.Pages{
+    public class ListaDuenosModel : PageModel{
+        private static IRepositorioDueno _repoDueno= new RepositorioDueno(new Persistencia.AppContext());
+
+        public IEnumerable<Dueno> listaDuenos {get;set;}
+
+        public void OnGet(){
+            listaDuenos=_repoDueno.GetAllDuenos();
         }
     }
 }
