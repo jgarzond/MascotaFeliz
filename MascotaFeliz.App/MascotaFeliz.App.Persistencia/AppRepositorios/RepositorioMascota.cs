3bab0bbda316a0ff7f4a3e806bcd9fafd318f585
jgarzond <jgarzond@unal.edu.co>
2022-09-12 19:31:16 -0500

    Version 1.0.4

---
+++ MascotaFeliz.App/MascotaFeliz.App.Persistencia/AppRepositorios/RepositorioMascota.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MascotaFeliz.App.Dominio;
+using Microsoft.EntityFrameworkCore;
+
+namespace MascotaFeliz.App.Persistencia
+{
+    public class RepositorioMascota : IRepositorioMascota
+    {
+        /// <summary>
+        /// Referencia al contexto de Dueno
+        /// </summary>
+        private readonly AppContext _appContext;
+        /// <summary>
+        /// Metodo Constructor Utiiza 
+        /// Inyeccion de dependencias para indicar el contexto a utilizar
+        /// </summary>
+        /// <param name="appContext"></param>//
+        
+        public RepositorioMascota(AppContext appContext)
+        {
+            _appContext = appContext;
+        }
+
+        public Dueno AddMascota(Mascota mascota)
+        {
+            var mascotaAdicionada = _appContext.Mascotas.Add(mascota);
+            _appContext.SaveChanges();
+            return mascotaAdicionada.Entity;
+        }
+
+        public void DeleteMascota(int idMascota)
+        {
+            var mascotaEncontrada = _appContext.Mascotas.FirstOrDefault(m => m.Id == idMascota);
+            if (mascotaEncontrada == null)
+                return;
+            _appContext.Mascotas.Remove(mascotaEncontrada);
+            _appContext.SaveChanges();
+        }
+
+       
+
+        public IEnumerable<Mascota> GetMascotasPorFiltro(string filtro)
+        {
+            var mascotas = GetAllMascotas(); // Obtiene todos los saludos
+            if (mascotas != null)  //Si se tienen saludos
+            {
+                if (!String.IsNullOrEmpty(filtro)) // Si el filtro tiene algun valor
+                {
+                    mascotas = mascotas.Where(m => m.Nombre.Contains(filtro));
+                }
+            }
+            return mascotas;
+        }
+
+        public IEnumerable<Mascota> GetAllMascotas()
+        {
+            return _appContext.Mascotas;
+        }
+
+        public Mascota GetMascota(int idMascota)
+        {
+            return _appContext.Mascotas.FirstOrDefault(m => m.Id == idMascota);
+        }
+
+        public Mascota UpdateMascota(Mascota mascota)
+        {
+            var mascotaEncontrada = _appContext.Mascotas.FirstOrDefault(m => m.Id == mascota.Id);
+            if (mascotaEncontrada != null)
+            {
+                mascotaEncontrada.Nombre = mascota.Nombre;
+                mascotaEncontrada.Color = mascota.Color;
+                mascotaEncontrada.Especie = mascota.Especie;
+                mascotaEncontrada.Raza = mascota.RAza;
+                
+                _appContext.SaveChanges();
+            }
+            return mascotaEncontrada;
+        }     
+    }
+}
