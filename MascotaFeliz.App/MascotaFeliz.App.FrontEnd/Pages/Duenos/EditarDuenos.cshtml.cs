15fdc264dc27654db532c9ca15780e8b149ff952
jgarzond <jgarzond@unal.edu.co>
2022-09-20 22:39:16 -0500

    Version 3.0

--- MascotaFeliz.App/MascotaFeliz.App.FrontEnd/Pages/Duenos/EditarDuenos.cshtml.cs
+++ MascotaFeliz.App/MascotaFeliz.App.FrontEnd/Pages/Duenos/EditarDuenos.cshtml.cs
@@ -4,13 +4,49 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MascotaFeliz.App.Dominio;
+using MascotaFeliz.App.Persistencia;
+
 
 namespace MascotaFeliz.App.FrontEnd.Pages
 {
     public class EditarDuenosModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly IRepositorioDueno _repoDueno;
+        [BindProperty]
+
+        public Dueno dueno{get;set;}
+        
+        public EditarDuenosModel(){
+            this._repoDueno = new RepositorioDueno(new Persistencia.AppContext());
+        }
+
+        public IActionResult OnGet (int? duenoId){
+            if(duenoId.HasValue){
+                dueno=_repoDueno.GetDueno(duenoId.Value);
+            }
+            else{
+                dueno = new Dueno();
+            }
+            if(dueno==null){
+                return RedirectToPage("./NotFound");
+            }
+            else{
+                return Page();
+            }
+        }
+
+        public IActionResult OnPost(){
+            if (!ModelState.IsValid){
+                return Page();
+            }
+            if(dueno.Id>0){
+                dueno=_repoDueno.UpdateDueno(dueno);
+            }
+            else{
+                _repoDueno.AddDueno(dueno);
+            }
+            return RedirectToPage("./ListaDuenos");
         }
     }
 }
--- MascotaFeliz.App/MascotaFeliz.App.FrontEnd/Pages/Mascotas/DetallesMascotas.cshtml.cs
+++ MascotaFeliz.App/MascotaFeliz.App.FrontEnd/Pages/Mascotas/DetallesMascotas.cshtml.cs
@@ -4,13 +4,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MascotaFeliz.App.Dominio;
+using MascotaFeliz.App.Persistencia;
 
 namespace MascotaFeliz.App.FrontEnd.Pages
 {
     public class DetallesMascotasModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioMascota _repoMascota;
+
+        public Mascota mascota {get;set;}
+
+        public DetallesMascotasModel(){
+            this._repoMascota= new RepositorioMascota(new Persistencia.AppContext());
+        }
+
+        public IActionResult OnGet(int mascotaId)
         {
+            mascota=_repoMascota.GetMascota(mascotaId);
+            if (mascota == null){
+                return RedirectToPage("./NotFound");
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
